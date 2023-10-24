5b584cf91462cea2287818c3877ee6eb2799913d
dev-tae <taehyun.kim0913@gmail.com>
2023-10-24 09:27:45 -0400

    Update Adminuser class

--- src/main/java/models/AdminUser.java
+++ src/main/java/models/AdminUser.java
@@ -15,4 +15,22 @@ public class AdminUser extends User {
     public void addBook(Book book) {
         Bookstore.getInstance().addBook(book);
     }
+
+    public void removeBook(Book book) {
+        Bookstore.getInstance().removeBook(book);
+    }
+
+    // Update book details (for simplicity, we're re-adding the book; in a real DB setup, this would be an update operation
+    public void updateBook(Book book) {
+        Bookstore.getInstance().removeBook(book);
+        Bookstore.getInstance().addBook(book);
+    }
+
+    // List all books
+    public void getAllBooks() {
+        for (Book book : Bookstore.getInstance().getAllBooks()) {
+            System.out.println(book);
+        }
+    }
+
 }
